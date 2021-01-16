a9f2800cbece7b95ae20d3f21bdd906806d991bb
mailatalamisan <76142191+mailatalamisan@users.noreply.github.com>
2021-01-16 15:33:23 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="section">
+	<div class="center grey-test">Copyright 2021 Maila Pizzas</div>
+</footer>
+
+</body>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<?php include('template/header.php'); ?>
+
+<?php include('template/footer.php'); ?>
+
+
+
+</html>
